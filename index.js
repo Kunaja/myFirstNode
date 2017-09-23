01b4b428387a886c3ab493a5f089bbff11f25347
Nattapat Klomjaemjit <kinlerbee38@gmail.com>
2017-09-23 10:36:07 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from './config/express';
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.PORT = process.env.PORT || 3000;
+var app = express();
+app.listen(process.env.PORT, () => {
+    console.log('Starting	node.js on	port	' + process.env.PORT);
+});
+module.exports = app;
+
+/*var express = require('express');
+var app = express();
+var bodyParser = require('body-parser')
+
+app.set('num', 1);
+var num = app.get('num');
+console.log(num); //1
+
+app.use(function (req, res, next) {
+    console.log(new Date().toString(), ':', req.path);
+    next();
+});
+
+app.use(bodyParser.json());
+
+app.get('/', function (req, res, next) {
+    res.send('Hello World!');
+});
+
+app.post('/', (req, res, next) => {
+    let body = req.body
+    res.send(`k ${body.a}`)
+})
+
+app.get('/help', function (req, res, next) {
+    res.send('Help me');
+});
+
+app.listen(3000, () => {
+    console.log('Starting node on port 3000')
+});*/
